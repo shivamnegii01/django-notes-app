a64579cfb58b1afcd5147dccf4f4051b1e58e0e2
shivamnegii01 <shivamsinghnegi143@gmail.com>
2026-01-31 19:54:13 +0530

    Update settings.py

--- notesapp/settings.py
+++ notesapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&dzi#zsb(hz6p(s#anunt&#-a%ohr2hld71*i72*^exvw-yq$y
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['13.127.64.250']
+ALLOWED_HOSTS = ['13.233.33.130']
 
 
 
